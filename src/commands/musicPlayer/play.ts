952f5ad9e52cc92581999572e919f1435b989f4a
Juan Alberto Palavecino <japc93k@gmail.com>
2024-05-01 02:16:35 +0200

    removed test

--- src/commands/musicPlayer/play.ts
+++ src/commands/musicPlayer/play.ts
@@ -4,7 +4,7 @@ import {
   Interaction,
   SlashCommandBuilder,
 } from "discord.js";
-import { QueryType, useMainPlayer } from "discord-player";
+import { useMainPlayer } from "discord-player";
 
 export default {
   data: new SlashCommandBuilder()
@@ -21,7 +21,6 @@ export default {
       const memberVoiceChannel = member.voice.channel;
 
       if (memberVoiceChannel) {
-        console.log("Joining voice channel");
         const player = useMainPlayer();
 
         if (interaction.isRepliable()) {
@@ -30,15 +29,8 @@ export default {
 
         if (interaction.isRepliable()) {
           const source = interaction.options.getString('source',true);
-          // const { track } = await player.play(memberVoiceChannel, source, {
-          //   nodeOptions: {
-          //     // nodeOptions are the options for guild node (aka your queue in simple word)
-          //     metadata: interaction, // we can access this metadata object using queue.metadata later on
-          //   },
-          // });
 
-          const { track } = await player.play(memberVoiceChannel, "./test.mp3", {
-            searchEngine: QueryType.FILE, // QueryType.FILE tells discord-player to play from our file system,
+          const { track } = await player.play(memberVoiceChannel, source, {
             nodeOptions: {
               // nodeOptions are the options for guild node (aka your queue in simple word)
               metadata: interaction, // we can access this metadata object using queue.metadata later on
